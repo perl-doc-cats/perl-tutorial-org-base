8dd0761769a5bccaf72e67bf1b524c7cc44febe3
Christian Walde <walde.christian@googlemail.com>
2011-10-29 00:12:04 +0200

    added a plugin to let some twitter attribs pass through the scrubber

---
+++ .ikiwiki/IkiWiki/Plugin/easehtmlscrubber.pm
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+package IkiWiki::Plugin::easehtmlscrubber;
+
+use warnings;
+use strict;
+use IkiWiki 3.00;
+
+
+sub import {
+    hook( type => "sanitize", id => "easehtmlscrubber", call => \&easehtmlscrubber, first => 1 );
+}
+
+sub getsetup () {
+    return (
+        plugin => {
+            description => "allow some twitter attribs to pass through the scrubber",
+            safe        => 1,
+            rebuild     => undef,
+        },
+    );
+}
+
+sub easehtmlscrubber {
+    my $scrubber = IkiWiki::Plugin::htmlscrubber::scrubber();
+
+    $scrubber->{_rules}{_}{$_} = 1 for qw( data-url data-text data-count );
+
+    return;
+}
+
+1;
